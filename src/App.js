02479d22f5b572d25163075429d3f467302acba3
zureenhanif <zureenhanif07@gmail.com>
2021-03-26 13:13:12 +1300

    test 4 2 passes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+import React, { Component } from 'react';
+ class App extends Component {
+ constructor() {
+ super();
+ this.state = {
+ count: 0,
+ }
+ }
+ makeIncrementer = amount => () =>
+ this.setState(prevState => ({
+ count: prevState.count + amount,
+ }));
+ increment = this.makeIncrementer(1);
+ render() {
+ return (
+ <div>
+   <p>Count: {this.state.count}</p>
+ <button className="increment" onClick={this.increment}>Increment
+count</button>
+ </div>
+ )
+ }
+ }
+ export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+import React from 'react';
+ import ReactDOM from 'react-dom';
+ import App from './App';
+ it('renders without crashing', () => {
+ const div = document.createElement('div');
+ ReactDOM.render(<App />, div);
+ ReactDOM.unmountComponentAtNode(div);
+ });
+ describe('Addition', () => {
+  it('knows that 2 and 2 make 4', () => {
+  expect(2 + 2).toBe(4);
+  });
+  });
